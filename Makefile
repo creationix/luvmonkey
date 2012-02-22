d27290395e62b767b82fb6b1d85efdb255ce803c
Tim Caswell <tim@creationix.com>
2012-02-22 15:22:03 -0600

    Initial commit

---
+++ Makefile
@@ -0,0 +1,57 @@
+OS_NAME=$(shell uname -s)
+ifeq (${OS_NAME},Darwin)
+LDFLAGS=-framework CoreServices
+else ifeq (${OS_NAME},Linux)
+LDFLAGS=-Wl,-E
+endif
+LDFLAGS+=-lmozjs185
+
+YAJLDIR=deps/yajl
+UVDIR=deps/uv
+HTTPDIR=deps/http-parser
+
+DEPS=${YAJLDIR}/yajl.a  \
+     ${UVDIR}/uv.a      \
+     ${HTTPDIR}/http_parser.o
+
+export Q=
+
+all: spiderluv
+
+${YAJLDIR}/CMakeLists.txt:
+	git submodule update --init ${YAJLDIR}
+
+${YAJLDIR}/Makefile: deps/Makefile.yajl ${YAJLDIR}/CMakeLists.txt
+	cp deps/Makefile.yajl ${YAJLDIR}/Makefile
+
+${YAJLDIR}/yajl.a: ${YAJLDIR}/Makefile
+	rm -rf ${YAJLDIR}/src/yajl
+	cp -r ${YAJLDIR}/src/api ${YAJLDIR}/src/yajl
+	$(MAKE) -C ${YAJLDIR}
+
+${UVDIR}/Makefile:
+	git submodule update --init ${UVDIR}
+
+${UVDIR}/uv.a: ${UVDIR}/Makefile
+	$(MAKE) -C ${UVDIR} uv.a
+
+${HTTPDIR}/Makefile:
+	git submodule update --init ${HTTPDIR}
+
+${HTTPDIR}/http_parser.o: ${HTTPDIR}/Makefile
+	${MAKE} -C ${HTTPDIR} http_parser.o
+
+spiderluv: build/main.o
+	$(CC) -g -o $@ build/main.o ${LDFLAGS}
+
+build/%.o: src/%.c ${DEPS}
+	mkdir -p build
+	$(CC) --std=c89 -D_GNU_SOURCE -g -Wall -Werror -c $< -o $@ -I${HTTPDIR} -I${UVDIR}/include -I${YAJLDIR}/src/api -I${YAJLDIR}/src -I/usr/include/js -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
+
+clean:
+	${MAKE} -C ${HTTPDIR} clean
+	${MAKE} -C ${YAJLDIR} clean
+	${MAKE} -C ${UVDIR} distclean
+	rm -rf build spiderluv
+
+.PHONY: all clean
---
+++ src/main.c
@@ -0,0 +1,66 @@
+/*
+ * This define is for Windows only, it is a work-around for bug 661663.
+ */
+#ifdef _MSC_VER
+# define XP_WIN
+#endif
+
+#include "jsapi.h"
+
+/* The class of the global object. */
+static JSClass global_class = {
+    "global", JSCLASS_GLOBAL_FLAGS,
+    JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_StrictPropertyStub,
+    JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, JS_FinalizeStub,
+    JSCLASS_NO_OPTIONAL_MEMBERS
+};
+
+/* The error reporter callback. */
+void reportError(JSContext *cx, const char *message, JSErrorReport *report)
+{
+    fprintf(stderr, "%s:%u:%s\n",
+            report->filename ? report->filename : "<no filename>",
+            (unsigned int) report->lineno,
+            message);
+}
+
+int main(int argc, const char *argv[])
+{
+    /* JS variables. */
+    JSRuntime *rt;
+    JSContext *cx;
+    JSObject  *global;
+
+    /* Create a JS runtime. */
+    rt = JS_NewRuntime(8L * 1024L * 1024L);
+    if (rt == NULL)
+        return 1;
+
+    /* Create a context. */
+    cx = JS_NewContext(rt, 8192);
+    if (cx == NULL)
+        return 1;
+    JS_SetOptions(cx, JSOPTION_VAROBJFIX | JSOPTION_JIT | JSOPTION_METHODJIT);
+    JS_SetVersion(cx, JSVERSION_LATEST);
+    JS_SetErrorReporter(cx, reportError);
+
+    /* Create the global object in a new compartment. */
+    global = JS_NewCompartmentAndGlobalObject(cx, &global_class, NULL);
+    if (global == NULL)
+        return 1;
+
+    /* Populate the global object with the standard globals,
+       like Object and Array. */
+    if (!JS_InitStandardClasses(cx, global))
+        return 1;
+
+
+    /* Your application code here. This may include JSAPI calls
+       to create your own custom JS objects and run scripts. */
+
+    /* Cleanup. */
+    JS_DestroyContext(cx);
+    JS_DestroyRuntime(rt);
+    JS_ShutDown();
+    return 0;
+}
