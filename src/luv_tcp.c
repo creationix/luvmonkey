c0db25e34763546d1d9e677a289477b1ca38e552
Tim Caswell <tim@creationix.com>
2012-02-23 21:22:37 -0600

    Start to embed uv struct in js class

--- src/luv_tcp.c
+++ src/luv_tcp.c
@@ -1,26 +1,47 @@
 #include "luv_tcp.h"
 
 static JSClass Tcp_class = {
-  "Tcp", 0,
+  "Tcp", JSCLASS_HAS_PRIVATE,
   JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_StrictPropertyStub,
   JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, JS_FinalizeStub,
   JSCLASS_NO_OPTIONAL_MEMBERS
 };
 
+/* TODO: Free the struct in the finalizer */
+
 static JSBool Tcp_constructor(JSContext *cx, uintN argc, jsval *vp) {
   JSObject* obj = JS_NewObject(cx, &Tcp_class, Tcp_prototype, NULL);
+
+  JS_SetPrivate(cx, obj, malloc(sizeof(uv_tcp_t)));
+
   JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(obj));
   return JS_TRUE;
 }
 
 
-static JSBool luv_bind(JSContext *cx, uintN argc, jsval *vp) {
-  printf("TODO: Implement luv_bind\n");
+static JSBool luv_tcp_bind(JSContext *cx, uintN argc, jsval *vp) {
+  JSObject* this = JS_THIS_OBJECT(cx, vp);
+  uv_tcp_t* handle;
+  handle = (uv_tcp_t*)JS_GetInstancePrivate(cx, this, &Tcp_class, NULL);
+
+  /* TODO: don't hardcode */
+  const char* host = "0.0.0.0";
+  int port = 8080;
+
+  struct sockaddr_in address = uv_ip4_addr(host, port);
+
+  if (uv_tcp_bind(handle, address)) {
+    uv_err_t err = uv_last_error(uv_default_loop());
+    JS_ReportError(cx, "uv_tcp_bind: %s", uv_strerror(err));
+    return JS_FALSE;
+  }
+
+  JS_SET_RVAL(cx, vp, JSVAL_VOID);
   return JS_TRUE;
 }
 
 static JSFunctionSpec Tcp_methods[] = {
-  JS_FS("bind", luv_bind, 0, JSPROP_ENUMERATE),
+  JS_FS("bind", luv_tcp_bind, 0, JSPROP_ENUMERATE),
   JS_FS_END
 };
 
