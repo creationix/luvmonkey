ae92afed900bed566f68918671fabb588765f89c
Tim Caswell <tim@creationix.com>
2012-03-08 00:18:07 -0600

    Fix tabs to spaces

--- test-tcp.js
+++ test-tcp.js
@@ -5,20 +5,20 @@ var Tcp = require('uv').Tcp;
 var server = new Tcp();
 server.bind("0.0.0.0", 1337);
 server.listen(128, function () {
-	var client = new Tcp();
-	server.accept(client);
-	client.readStart();
-	client.onData = function (chunk) {
-		p("onData", chunk);
-		client.write(chunk, function () {
-			p("onWrite", chunk);
-		});
-	};
-	client.onEnd = function () {
-		p("onEnd");
-		client.close();
-	};
-	p("Connection!", client);
+  var client = new Tcp();
+  server.accept(client);
+  client.readStart();
+  client.onData = function (chunk) {
+    p("onData", chunk);
+    client.write(chunk, function () {
+      p("onWrite", chunk);
+    });
+  };
+  client.onEnd = function () {
+    p("onEnd");
+    client.close();
+  };
+  p("Connection!", client);
 });
 
 p(server);
